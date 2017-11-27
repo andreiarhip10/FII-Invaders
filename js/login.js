dcb9c25806437b99b2e6e351fbced0fd545791a6
andreiarhip10 <andrei.arhip10@yahoo.com>
2017-11-27 22:02:58 +0200

    added login page draft

---
+++ js/login.js
@@ -0,0 +1,13 @@
+function hover() {
+    element = document.getElementsByClassName('fb2');
+    element[0].setAttribute('src', 'fb1.svg');
+}
+
+function unhover(){
+    element = document.getElementsByClassName('fb2');
+    element[0].setAttribute('src','fb2.svg');
+}
+
+function background(){
+    
+}
